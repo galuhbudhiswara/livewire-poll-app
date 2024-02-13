4a08ba58c1b907a041959910167bb375317da9c0
galuhbudhiswara <galuhbudhiswara280705@gmail.com>
2024-02-13 15:20:58 +0700

    actions in livewire

---
+++ app/Livewire/CreatePoll.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CreatePoll extends Component
+{
+    public $title;
+    public $options = [''];
+    public function render()
+    {
+        return view('livewire.create-poll');
+    }
+
+     public function AddOption() {
+        $this->options[] = '';
+    }
+
+    // public function mount() {
+
+    // }
+}
